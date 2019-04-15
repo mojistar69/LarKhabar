9650d1d8feeda4ffb1fa2337fbf0accdd8eb7fdb
Mojtaba Mesri <m.mesri69@gmail.com>
2019-04-15 15:34:20 +0430

    اضافه کردن دیتابیس
    اضافه کردن صفحه تماس کلی

--- views/post/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace app\models;
-use Yii;
-?>
-<h1>PostForm</h1>
-<img src="http://lorempixel.com/<?=Yii::$app->params['pic_size']['w']?>/<?=Yii::$app->params['pic_size']['h']?>/sports/2/">
